c60e08d7472251a58dd0066b70dc83c208c38c6c
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2024-12-13 08:41:37 -0300

    feat(day05): adiciona parte 2

--- src/test/java/com/ferraz/day05/Day05Test.java
+++ src/test/java/com/ferraz/day05/Day05Test.java
@@ -20,15 +20,6 @@ public class Day05Test {
     }
 
     @Test
-    public void testPart2Sample() throws IOException {
-        Day day = new Day05(true);
-        long initialTime = System.currentTimeMillis();
-        long answer = day.part2();
-        printResults(answer, initialTime);
-        Assert.assertEquals(0, answer);
-    }
-
-    @Test
     public void testPart1() throws IOException {
         Day day = new Day05();
         long initialTime = System.currentTimeMillis();
@@ -38,12 +29,21 @@ public class Day05Test {
     }
 
     @Test
+    public void testPart2Sample() throws IOException {
+        Day day = new Day05(true);
+        long initialTime = System.currentTimeMillis();
+        long answer = day.part2();
+        printResults(answer, initialTime);
+        Assert.assertEquals(123, answer);
+    }
+
+    @Test
     public void testPart2() throws IOException {
         Day day = new Day05();
         long initialTime = System.currentTimeMillis();
         long answer = day.part2();
         printResults(answer, initialTime);
-        Assert.assertEquals(0, answer);
+        Assert.assertEquals(6319, answer);
     }
 
 }
